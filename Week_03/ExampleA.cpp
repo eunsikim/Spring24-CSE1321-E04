3bef7df6c7e8d37a6563857883d50cb70bf9462b
eunsikim <kimeunsik229@gmail.com>
2024-01-22 19:41:07 -0500

    Week 3 examples

---
+++ Week_03/ExampleA.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+    // Constant Variable
+    const int PI = 3.14;
+
+    int number1 = 8;
+    float number2 = 2.5;
+
+    cout << number1 + number2 << endl;
+
+    // Truncate
+    int res = number1 + number2;
+
+    cout << res << endl;
+}
