c56f215c474c4d14fded728953e80ed36324bd73
eunsikim <kimeunsik229@gmail.com>
2024-02-12 19:24:30 -0500

    RNG and loop examples

--- Week_06/RNG_2.cpp
+++ Week_06/RNG_2.cpp
@@ -8,9 +8,9 @@ int main()
 {
     srand((unsigned)time(0));
     int randomNumber;
-    // Generate random integers in range 0 to 9
+    // Generate random integers in range 5 to 10
     for(int i = 0; i < 10; i++){
-        randomNumber = (rand() % 10);
+        randomNumber = (rand() % 5 + 1) + 5;
         cout << randomNumber << endl;
     }
 
---
+++ Week_06/doWhileLoop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+
+    do{
+        cout << "i = " << i << endl;
+        i++;
+    }while(i < 10);
+}
