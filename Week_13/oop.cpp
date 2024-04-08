903ff93241fb48e877ad8a83c27b499800f56104
eunsikim <kimeunsik229@gmail.com>
2024-04-08 19:02:25 -0400

    OOP example

---
+++ Week_13/oop.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+class phone
+{
+public:
+    string color;
+    string model;
+    string year;
+
+    // Constructor
+    phone()
+    {
+        color = "white";
+        model = "Apple";
+        year = "2024";
+    }
+
+    phone(string color, string model, string year)
+    {
+        this->color = color;
+        this->model = model;
+        this->year = year;
+    }
+
+    void play(string gameName)
+    {
+        cout << "Opening " << gameName << endl;
+        call("98654321");
+    }
+    void call(string number)
+    {
+        cout << "Calling " << number << endl;
+    }
+};
+
+int main()
+{
+    phone stock[10];
+
+    stock[0] = phone("black", "Samsung", "2022");
+
+    // cout << stock[0].model << " " << stock[0].year << " " << stock[0].color << endl;
+
+    stock[1] = phone();
+
+    cout << stock[0].model << " " << stock[1].year << " " << stock[1].color << endl;
+
+    for (phone p : stock)
+    {
+        cout << p.model << " " << p.year << " " << p.color << endl;
+    }
+}
